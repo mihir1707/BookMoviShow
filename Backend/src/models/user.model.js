4386c1967868baba11e10dab886c50dba36dfa0f
Mihir Khunt <mihirkhunt2006@gmail.com>
2026-02-16 18:36:11 +0530

    add bcryptjs

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 
 const userSchema = new mongoose.Schema({
@@ -51,9 +51,9 @@ const userSchema = new mongoose.Schema({
 
 // Hash password
 userSchema.pre('save', async function (next) {
-    if(!this.isModified('password')) return next;
+    if(!this.isModified('password')) return next();
     this.password = await bcrypt.hash(this.password, 10)
-    next;
+    next();
 })
 
 
