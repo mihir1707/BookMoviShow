cee14804dddac30d4ebc9416294b2bdaa9cb6b0d
Mihir Khunt <mihirkhunt2006@gmail.com>
2026-03-05 10:57:50 +0530

    firebase change

--- Frontend/src/firebase.js
+++ Frontend/src/firebase.js
@@ -1,21 +1,21 @@
-import { initializeApp } from "firebase/app";
+// import { initializeApp } from "firebase/app";
 
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+// import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCgfppvEVi4mhgSFzJTsm4b7IKZgTOxKM8",
-    authDomain: "bookmovishow.firebaseapp.com",
-    projectId: "bookmovishow",
-    storageBucket: "bookmovishow.firebasestorage.app",
-    messagingSenderId: "346188799454",
-    appId: "1:346188799454:web:2e847da1c31b87c495914f"
-};
+// const firebaseConfig = {
+//     apiKey: "AIzaSyCgfppvEVi4mhgSFzJTsm4b7IKZgTOxKM8",
+//     authDomain: "bookmovishow.firebaseapp.com",
+//     projectId: "bookmovishow",
+//     storageBucket: "bookmovishow.firebasestorage.app",
+//     messagingSenderId: "346188799454",
+//     appId: "1:346188799454:web:2e847da1c31b87c495914f"
+// };
 
 
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
+// export const auth = getAuth(app);
 
-export const googleProvider = new GoogleAuthProvider();
+// export const googleProvider = new GoogleAuthProvider();
 
-export default app;
+// export default app;
